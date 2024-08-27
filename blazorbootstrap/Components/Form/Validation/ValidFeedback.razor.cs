4d16643a406f5a5392e0439bb823c51d65f981ec
Suiram1701 <110390261+Suiram1701@users.noreply.github.com>
2024-08-27 13:48:11 +0200

    Added validation components -Feature: Added ValidationFieldClassProvider that provides the FieldCssClass for an EditContext. -Feature: Added ValidationFeedback component. -Feature: Added ValidFeedback component. -Improvement: Used the validation components in the existing form demos.

---
+++ blazorbootstrap/Components/Form/Validation/ValidFeedback.razor.cs
@@ -0,0 +1,56 @@
+﻿namespace BlazorBootstrap;
+
+public partial class ValidFeedback<TValue> : BlazorBootstrapComponentBase
+{
+    [Parameter]
+    public Expression<Func<TValue>> For { get; set; } = default!;
+
+    [Parameter]
+    public string Message { get; set; } = default!;
+
+    [Parameter]
+    public bool AsTooltip { get; set; }
+
+    [CascadingParameter]
+    private EditContext EditContext { get; set; } = default!;
+
+    private FieldIdentifier _fieldIdentifier;
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        if (EditContext is null)
+        {
+            throw new InvalidOperationException($"The {GetType()} component have to be placed inside of an {typeof(EditForm)}.");
+        }
+
+        _fieldIdentifier = FieldIdentifier.Create(For);
+    }
+
+    protected override void OnInitialized()
+    {
+        base.OnInitialized();
+        EditContext.OnValidationStateChanged += EditContext_OnValidationStateChanged;
+    }
+
+    private void EditContext_OnValidationStateChanged(object? sender, ValidationStateChangedEventArgs e)
+    {
+        StateHasChanged();
+    }
+
+    protected override string? ClassNames => BuildClassNames(
+        Class,
+        ("valid-feedback", !AsTooltip),
+        ("valid-tooltip", AsTooltip));
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+
+        if (disposing)
+        {
+            EditContext.OnValidationStateChanged -= EditContext_OnValidationStateChanged;
+        }
+    }
+}
---
+++ blazorbootstrap/Components/Form/Validation/ValidationFieldClassProvider.cs
@@ -0,0 +1,79 @@
+﻿namespace BlazorBootstrap;
+
+public class ValidationFieldClassProvider : ComponentBase, IDisposable
+{
+    [Parameter]
+    public bool DisableValidState { get; set; }
+
+    [CascadingParameter]
+    private EditContext EditContext { get; set; } = default!;
+
+    private readonly BootstrapCssClassProvider _provider = new();
+    private bool disposedValue;
+
+    protected override void OnParametersSet()
+    {
+        if (EditContext is null)
+        {
+            throw new InvalidOperationException($"The {GetType()} component have to be placed inside of an {typeof(EditForm)}.");
+        }
+
+        _provider.DisableValidState = DisableValidState;
+    }
+
+    protected override void OnInitialized()
+    {
+        EditContext.SetFieldCssClassProvider(_provider);
+        EditContext.OnValidationRequested += EditContext_OnValidationRequested;
+    }
+
+    private void EditContext_OnValidationRequested(object? sender, ValidationRequestedEventArgs e)
+    {
+        _provider.FirstTimeValidated = true;
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposedValue)
+        {
+            if (disposing)
+            {
+                EditContext.OnValidationRequested -= EditContext_OnValidationRequested;
+            }
+
+            disposedValue = true;
+        }
+    }
+
+    public void Dispose()
+    {
+        // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        Dispose(disposing: true);
+        GC.SuppressFinalize(this);
+    }
+
+    private class BootstrapCssClassProvider : FieldCssClassProvider
+    {
+        public bool DisableValidState { get; set; }
+
+        public bool FirstTimeValidated { get; set; }
+
+        public override string GetFieldCssClass(EditContext editContext, in FieldIdentifier fieldIdentifier)
+        {
+            if (!(editContext.IsModified(fieldIdentifier) || FirstTimeValidated))     // Show only a state if the field was modified or a validation was requested first time.
+            {
+                return string.Empty;
+            }
+            else if (!editContext.GetValidationMessages(fieldIdentifier).Any())
+            {
+                return !DisableValidState
+                    ? "is-valid"
+                    : string.Empty;
+            }
+            else
+            {
+                return "is-invalid";
+            }
+        }
+    }
+}
